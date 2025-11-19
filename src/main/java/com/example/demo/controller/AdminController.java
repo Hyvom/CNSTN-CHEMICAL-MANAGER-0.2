51b44670003382aaf3f9591f548f10dc771df232
Hyvom <hyvomrahmouni00@gmail.com>
2025-11-19 21:45:23 +0100

    attempting to fix forbidden product view error

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/admins")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AdminController {
 
     private final AdminService adminService;
--- src/main/java/com/example/demo/controller/RequestController.java
+++ src/main/java/com/example/demo/controller/RequestController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/requests")
+@CrossOrigin(origins = "http://localhost:4200")
 public class RequestController {
 
     private final RequestService requestService;
